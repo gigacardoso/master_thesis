c77c5e13936952dbb6a6acb8a791445933f1a1a5
gigacardoso <daniel.cardoso18@live.com>
2014-03-18 21:22:01 +0000

    finished remover missing data

--- Tese/src/utils/MissingRemover.java
+++ Tese/src/utils/MissingRemover.java
@@ -5,25 +5,37 @@ import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class MissingRemover {
-	
+
 	private static String[] exams = {"GPT","GOT","ZTT","TTT","T-BIL","D-BIL","I-BIL","ALB","CHE","T-CHO","TP","Type","Activity"};	
 	private static String data = "C:\\hepat_data030704\\";
 	private static String path = data +"data\\";
-	
+	private static String outPath = "C:\\Users\\Daniel\\Documents\\GitHub\\HMMinR\\";
+
 	public static void main(String[] args) {
-		for(int i= 0; i< exams.length;i++){
-			try {
+		try {
+			for(int i= 0; i< exams.length;i++){
 				BufferedReader in = new BufferedReader(new FileReader(path+exams[i]+".csv"));
-				BufferedWriter out = new BufferedWriter(new FileWriter(path+exams[i]+".csv"));
+				BufferedWriter out = new BufferedWriter(new FileWriter(outPath+exams[i]+".csv"));
+				String line = in.readLine();
+				out.write(line + "\n");
+				int size = line.split(",",-1).length;
+				String[] split;
+				while((line = in.readLine()) != null){
+					split = line.split(",");
+					if(split.length == size){
+						out.write(line + "\n");
+					}
+				}
 				in.close();
 				out.close();
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
-			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
